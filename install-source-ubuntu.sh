4d62dd658adccf3dc1611a88a09d5acb34772e7e
dkwon116 <dkwon86116@gmail.com>
2022-10-01 21:00:04 +0900

    use dan branch

--- install-source-ubuntu.sh
+++ install-source-ubuntu.sh
@@ -10,6 +10,7 @@ export PYTHON="$(pwd)/miniconda3/envs/hummingbot/bin/python3"
 # INSTALL HUMMINGBOT
 # 3) Clone Hummingbot
 git clone https://github.com/dkwon116/hummingbot.git
+git checkout dan
 # 4) Install Hummingbot
 export hummingbotPath="$(pwd)/hummingbot" && cd $hummingbotPath && ./install
 # 5) Activate environment and compile code
